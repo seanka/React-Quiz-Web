054ce7098ec21ca427972a48849e17a759a7c140
seanka <and.seanka@gmail.com>
2023-12-18 20:27:24 +0700

    Adde start status logic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -20,6 +21,9 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
 
+    case "start":
+      return { ...state, status: "active" };
+
     default:
       throw new Error("Action unknown");
   }
@@ -32,7 +36,7 @@ export default function App() {
 
   useEffect(() => {
     fetch("http://localhost:9009/questions")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
@@ -44,7 +48,8 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
---
+++ src/Question.js
@@ -0,0 +1,5 @@
+function question() {
+  return <div></div>;
+}
+
+export default question;
