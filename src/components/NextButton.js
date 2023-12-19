82b8df2981e48c66dc7753a4dee72a87540ee3ef
seanka <and.seanka@gmail.com>
2023-12-19 08:20:30 +0700

    Added finished quiz logic

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,10 +1,20 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <button className="btn btn-ui" onClick={() => dispatch({ type: "nextQuestion" })}>
-      Next
-    </button>
-  );
+
+  if (index < numQuestions - 1) {
+    return (
+      <button className="btn btn-ui" onClick={() => dispatch({ type: "nextQuestion" })}>
+        Next
+      </button>
+    );
+  }
+  if (index === numQuestions - 1) {
+    return (
+      <button className="btn btn-ui" onClick={() => dispatch({ type: "finish" })}>
+        Next
+      </button>
+    );
+  }
 }
 
 export default NextButton;
